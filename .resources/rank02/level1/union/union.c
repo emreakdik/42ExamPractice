ca3d2cbedcbdb40795e2ddc6f9c127900a3ab4bd
Emre Akdik <98621456+emreakdik@users.noreply.github.com>
2023-03-17 02:36:42 +0300

    Update union.c

--- .resources/rank02/level1/union/union.c
+++ .resources/rank02/level1/union/union.c
@@ -1,25 +1,43 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int check(int c, char *str, int index)
 {
+	int i = 0;
+	
+	while(i < index)
+	{
+		if (str[i] == c)
+			return 0;
+		i++;
+	}
+	return  1;
+}
+
+int main(int argc, char **argv)
+{	
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	
 	if (argc == 3)
 	{
-		int	ascii[255] = {0};
-		
-		for (int i = 0; i < 3; i += 1)
+		while(argv[1][i] != '\0')
 		{
-			int j = 0;
-			while (argv[i][j])
-			{
-				if (ascii[(int)argv[i][j]] == 0)
-				{
-					ascii[(int)argv[i][j]] = 1;
-					write(1, &argv[i][j], 1);
-				}
-				j += 1;
-			}
+			i++;
+		}
+		while(argv[2][j] != '\0')
+		{
+			argv[1][i] = argv[2][j];
+			i++;
+			j++;
+		}
+		i--;
+		while(k <= i)
+		{
+			if(check(argv[1][k], argv[1], k) == 1) 
+				write (1, &argv[1][k], 1); 
+			k++;
 		}
 	}
-	write(1, "\n", 1);
-	return (0);
+	write (1, "\n", 1);
 }
