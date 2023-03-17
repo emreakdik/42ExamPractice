7d6940b0ca956f35f8f84b53b670b0499fada417
Emre Akdik <98621456+emreakdik@users.noreply.github.com>
2023-03-17 10:12:27 +0300

    Update tester.sh

--- .resources/rank02/level2/ft_atoi_base/tester.sh
+++ .resources/rank02/level2/ft_atoi_base/tester.sh
@@ -25,8 +25,8 @@ file2=../../../../rendu/ft_atoi_base/ft_atoi_base.c
     gcc -o out1 "$file1" main.c
     gcc -o out2 "$file2" main.c
 
-    ./out1 "See? It's easy to print the same thing" > out1.txt
-    ./out2 "See? It's easy to print the same thing" > out2.txt
+    ./out1 "Ceci permet de decouvrir le fonctionnement de ton ft_atoi_base." "16" > out1.txt
+    ./out2 "Ceci permet de decouvrir le fonctionnement de ton ft_atoi_base." "16" > out2.txt
 
     if ! diff -q out1.txt out2.txt >/dev/null ; then
         out1=$(cat out1.txt)
@@ -43,8 +43,8 @@ file2=../../../../rendu/ft_atoi_base/ft_atoi_base.c
     gcc -o out1 "$file1" main.c
     gcc -o out2 "$file2" main.c
 
-    ./out1 " this        time it      will     be    more complex  . " > out1.txt
-    ./out2 " this        time it      will     be    more complex  . " > out2.txt
+    ./out1 "13268!" "16" > out1.txt
+    ./out2 "13268!" "16" > out2.txt
 
     if ! diff -q out1.txt out2.txt >/dev/null ; then
         out1=$(cat out1.txt)
@@ -60,74 +60,8 @@ file2=../../../../rendu/ft_atoi_base/ft_atoi_base.c
     gcc -o out1 "$file1" main.c
     gcc -o out2 "$file2" main.c
 
-    ./out1 "No S*** Sherlock..." "nAw S*** ShErLaWQ..." > out1.txt
-    ./out2 "No S*** Sherlock..." "nAw S*** ShErLaWQ..." > out2.txt
-
-    if ! diff -q out1.txt out2.txt >/dev/null ; then
-        out1=$(cat out1.txt)
-        out2=$(cat out2.txt)
-        echo "$(tput setaf 1)$(tput bold)FAIL$(tput sgr 0)"
-        echo "${GREEN}Expected Output:${RESET} \"$out1\""
-        echo "${RED}Your Output:${RESET}     \"$out2\""
-        rm out1 out2 out1.txt out2.txt
-        exit 1
-    fi
-
-
-# 5. test 
-    gcc -o out1 "$file1" main.c
-    gcc -o out2 "$file2" main.c
-
-    ./out1 "comme c'est cocasse" "vous avez entendu, Mathilde ?" > out1.txt
-    ./out2 "comme c'est cocasse" "vous avez entendu, Mathilde ?" > out2.txt
-
-    if ! diff -q out1.txt out2.txt >/dev/null ; then
-        out1=$(cat out1.txt)
-        out2=$(cat out2.txt)
-        echo "$(tput setaf 1)$(tput bold)FAIL$(tput sgr 0)"
-        echo "${GREEN}Expected Output:${RESET} \"$out1\""
-        echo "${RED}Your Output:${RESET}     \"$out2\""
-        rm out1 out2 out1.txt out2.txt
-        exit 1
-    fi
-
-# 6. test
-    gcc -o out1 "$file1" main.c
-    gcc -o out2 "$file2" main.c
-
-    ./out1 "5" > out1.txt
-    ./out2 "5" > out2.txt
-
-    if ! diff -q out1.txt out2.txt >/dev/null ; then
-        out1=$(cat out1.txt)
-        out2=$(cat out2.txt)
-        echo "$(tput setaf 1)$(tput bold)FAIL$(tput sgr 0)"
-        echo "${GREEN}Expected Output:${RESET} \"$out1\""
-        echo "${RED}Your Output:${RESET}     \"$out2\""
-        rm out1 out2 out1.txt out2.txt
-        exit 1
-    fi
-
-# 7. test
-    gcc -o out1 "$file1" main.c
-    gcc -o out2 "$file2" main.c
-
-    ./out1 "Too" "Many" "Arguments" > out1.txt
-    ./out2 "Too" "Many" "Arguments" > out2.txt
-
-    if ! diff -q out1.txt out2.txt >/dev/null ; then
-        out1=$(cat out1.txt)
-        out2=$(cat out2.txt)
-        echo "$(tput setaf 1)$(tput bold)FAIL$(tput sgr 0)"
-        echo "${GREEN}Expected Output:${RESET} \"$out1\""
-        echo "${RED}Your Output:${RESET}     \"$out2\""
-        rm out1 out2 out1.txt out2.txt
-        exit 1
-    fi
-
-# 8. test
-    gcc -o out1 "$file1" main.c
-    gcc -o out2 "$file2" main.c
+    ./out1 "-13268!" "10" > out1.txt
+    ./out2 "-13268!" "10" > out2.txt
 
     if ! diff -q out1.txt out2.txt >/dev/null ; then
         out1=$(cat out1.txt)
