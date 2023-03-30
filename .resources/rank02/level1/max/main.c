f89ea274b0bf0a846c2757b5345f595b05b1c5db
Emre Akdik <98621456+emreakdik@users.noreply.github.com>
2023-03-31 00:13:19 +0300

    Update main.c

--- .resources/rank02/level1/max/main.c
+++ .resources/rank02/level1/max/main.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
     int *tab = malloc(sizeof(int) * (argc - 1));
     
     printf("max( {");
-    for (unsigned int i = 2; i < argc; i++)
+    for (int i = 2; i < argc; i++)
     {
         printf("%s", argv[i]);
         if (i != argc - 1)
